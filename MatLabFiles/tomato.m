d512c55d9399a34891514e57835a0e99714c35c5
erolley-parnell <31401697+erolley-parnell@users.noreply.github.com>
2018-10-16 11:14:50 +0100

    Update tomato.m

--- MatLabFiles/tomato.m
+++ MatLabFiles/tomato.m
@@ -2,19 +2,44 @@ classdef tomato < handle
     properties
         Z = [];
         string = [];
-        arrayData;
+        arrayData = [];
+        tomatoOrder = [0,1,2; 3,4,5; 6,7,8; 9,10,11; 12,13,14];
+        serialOrder = [4,9,14; 3,8,13; 2,7,12; 1,6,11; 0,5,10];
+        %N is the number of the real world position
+        %Tomato is shaped [0,1,2;
+        %                  3,4,5;
+        %                  6,7,8;
+        %                  9,10,11;
+        %                  12,13,14]
+        %String is shaped [4,9,14;
+        %                  3,8,13;
+        %                  2,7,12;
+        %                  1,6,11;
+        %                  0,5,10]
     end
     methods
+        function order(obj)
+            for(i = 1:1:5)
+                for(l = 1:1:3)
+                n = obj.tomatoOrder(l,i)
+                [j,k] = find(obj.serialOrder == n)
+                obj.Z(l,i) = obj.arrayData(j,k)    
+                end
+            end
+        end
         function prep(obj)
-            obj.arrayData = str2num(obj.string);
-            numel(obj.arrayData);
+            obj.arrayData = str2num(obj.string)
+            %numel(obj.arrayData);
+            %length(obj.arrayData);
             if length(obj.arrayData) == 20
+                %obj.order
                 obj.Z = reshape(obj.arrayData, [4,5]);
                 obj.Z = obj.Z(:,any(obj.Z));
             else
-                fprintf('Serial Issue')
+                fprintf('Serial Issue \n')
             end
         end
+
         function cleanup(obj)
             clear obj.Z
             clear obj.string
