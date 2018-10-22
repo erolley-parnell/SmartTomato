ca67d0458f7b66f54be7ff0e01b652e22017a80e
erolley-parnell <31401697+erolley-parnell@users.noreply.github.com>
2018-10-22 17:41:02 +0100

    Update tomato.m

--- MatLabFiles/tomato.m
+++ MatLabFiles/tomato.m
@@ -3,8 +3,7 @@ classdef tomato < handle
         Z = [];
         string = [];
         arrayData = [];
-        tomatoOrder = [0,1,2; 3,4,5; 6,7,8; 9,10,11; 12,13,14];
-        serialOrder = [4,9,14; 3,8,13; 2,7,12; 1,6,11; 0,5,10];
+        serialOrder = [5,10,15; 4,9,14; 3,8,13; 2,7,12; 1,6,11];
         %N is the number of the real world position
         %Tomato is shaped [0,1,2;
         %                  3,4,5;
@@ -19,22 +18,20 @@ classdef tomato < handle
     end
     methods
         function order(obj)
+            %Reorder the data so that the order specified in serialOrder
             for(i = 1:1:5)
                 for(l = 1:1:3)
-                n = obj.tomatoOrder(l,i)
-                [j,k] = find(obj.serialOrder == n)
-                obj.Z(l,i) = obj.arrayData(j,k)    
+                    n = obj.serialOrder(i,l);
+                    obj.Z(i,l) = obj.arrayData(n);   
                 end
             end
         end
         function prep(obj)
             obj.arrayData = str2num(obj.string)
-            %numel(obj.arrayData);
-            %length(obj.arrayData);
             if length(obj.arrayData) == 20
-                %obj.order
-                obj.Z = reshape(obj.arrayData, [4,5]);
-                obj.Z = obj.Z(:,any(obj.Z));
+                obj.order
+                %obj.Z = reshape(obj.arrayData, [4,5]);
+                %obj.Z = obj.Z(:,any(obj.Z));
             else
                 fprintf('Serial Issue \n')
             end
